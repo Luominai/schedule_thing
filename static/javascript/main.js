75ac08feaac67db10336e10a64fe2162ee9f8b9c
KevinLi0711 <108951684+Luominai@users.noreply.github.com>
2023-09-14 14:06:44 -0400

    why am i so bothered by the boxes covering the lines

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -7,7 +7,7 @@ function getBlock(day, startHour, column) {
 function colorBlock(block, color, before, percent) {
 
     block.setAttribute("style", `background: linear-gradient(to bottom, white 0% ${before}%, ${color} ${before}% ${before + percent}%, white ${before + percent}% 100%); 
-    padding:0; padding-top:1`)
+    padding:0; padding-top:0; margin-top:1`)
 }
 
 function colorTime(startTime, endTime) {
